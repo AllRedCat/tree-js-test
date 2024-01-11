12ad81b572337353635dfe9df4ad07d17b9a9bdb
AllRedCat <leonismogabriel@gmail.com>
2024-01-10 23:58:23 -0300

    Add light point to the object

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './style.css'
 // Importar 'Three.js'
 import * as THREE from 'three'
 
-// Criando sena
+// Criando cena
 const scene = new THREE.Scene()
 
 // Criando camera
@@ -25,12 +25,21 @@ camera.position.setZ(30)
 // renderer.render( scene, camera )
 
 // Objeto Geométrico
-const geometry = new THREE.TorusGeometry( 6, 2, 16, 100 ) // Obejeto
-const material = new THREE.MeshBasicMaterial( { color: 0x9E4770 , wireframe: true } ) //Material
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 ) // Obejeto
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } ) //Material que reage a luz
 const torus = new THREE.Mesh( geometry, material ) // Criando 'mesh' combinando o Objeto geométrico e o material
 
-// Adicionando Obejato à sena
+// Adicionando Obejato à cena
 scene.add(torus)
+
+// Ponto de luz
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20,20,20)
+
+// Ponto de luz ambiente | Ilumina todos objetos em cena
+
+scene.add(pointLight)
+
 
 // Função para rederizar o objeto automaticamente
 function animate() {
@@ -38,7 +47,7 @@ function animate() {
 
   // Animando o objeto
   torus.rotation.x += 0.01
-  torus.rotation.y += 0.008
+  torus.rotation.y += 0.005
   torus.rotation.z += 0.01
 
 
